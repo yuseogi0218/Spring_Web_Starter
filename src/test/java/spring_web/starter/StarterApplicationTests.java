12ad790258e3d29bc64218c58f555dcdc7e025fe
yuseogi0218 <yuseogi0218@gmail.com>
2021-07-27 16:14:37 +0900

    init

---
+++ src/test/java/spring_web/starter/StarterApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_web.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
